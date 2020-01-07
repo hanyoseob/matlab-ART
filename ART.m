f8d30dc9c4b505f7cccb041ec1c01c898dcbe3a2
hanyoseob <hanyoseob@kaist.ac.kr>
2020-01-06 21:13:32 -0700

    Upload init

--- ART.m
+++ ART.m
@@ -2,37 +2,31 @@
 % https://en.wikipedia.org/wiki/Algebraic_reconstruction_technique
 
 %%
-function x  = ART(A,AT,b,x,lambda,n,bpos,bfig)
-
-if (nargin < 8)
-    bfig	= false;
-end
+function x  = ART(A,AT,b,x,lambda,niter,bpos)
 
 if (nargin < 7)
     bpos	= true;
 end
 
 if (nargin < 6)
-    n       = 1e2;
+    niter       = 1e2;
 end
 
 ATA	= AT(A(ones(size(x), 'single')));
 
-for i = 1:n
+for i = 1:niter
     
     x  	= x + lambda*AT(b - A(x))./ATA;
     
     if (bpos)
         x(x < 0) = 0;
     end
-    
-    if (bfig)
-        figure(1); colormap gray;
-        imagesc(x);
-        axis image off;
-         title([num2str(i) ' / ' num2str(n)]);
-        drawnow();
-    end
+
+    figure(1); colormap gray;
+    imagesc(x);
+    axis image off;
+    title([i, niter], '%d / %d']);
+    drawnow();
 end
 
 x   = gather(x);
