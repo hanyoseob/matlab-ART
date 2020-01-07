b7eaa78d748d5427139e9bce2f9cb1750aeb6ee4
hanyoseob <hanyoseob@kaist.ac.kr>
2020-01-06 21:10:57 -0700

    Upload init

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# algebraic-reconstruction-technique
+# ART
 
-## Reference
-Algebraic Reconstruction Technique (ART)
-- https://en.wikipedia.org/wiki/Algebraic_reconstruction_technique
+### Title
+[Algebraic Reconstruction Technique (ART)](https://en.wikipedia.org/wiki/Algebraic_reconstruction_technique)
 
-## Algebraic Reconstruction Technique (ART)
-The [`algebraic reconstruction technique (ART)`]( https://en.wikipedia.org/wiki/Algebraic_reconstruction_technique) is a class of `iterative algorithms` used in [computed tomography](https://en.wikipedia.org/wiki/CT_scan). 
+### Abstract
+The **algebraic reconstruction technique (ART)** is an [iterative reconstruction](https://en.wikipedia.org/wiki/Iterative_reconstruction) technique used in [computed tomography](https://en.wikipedia.org/wiki/CT_scan). It reconstructs an image from a series of angular projections (a sinogram). Gordon, Bender and Herman first showed its use in image reconstruction; whereas the method is known as Kaczmarz method in numerical linear algebra.
 
-An advantage of ART over other reconstruction methods ( such as [`filtered backprojection (FBP)`](https://en.wikipedia.org/wiki/Radon_transform) which is deterministic method ) is that it is relatively easy to incorporate prior knowledge into the reconstruction process.
+An advantage of ART over other reconstruction methods (such as [filtered backprojection (FBP)](https://en.wikipedia.org/wiki/Radon_transform)) is that it is relatively easy to incorporate prior knowledge into the reconstruction process.
 
-        x^(k+1) = x^(k) + lambda_(k) * AT( y - A( x ) ) / AT( A( ones(size(x)) ) )
-        
-        where,  A() is radon transform ( called by projection ) 
-        
-                AT() is inverse radon transform without filtration ( called by backprojection ).
+
+## Equation
+![alt text](./img/eq_ART.png "Equation of ART")
+
+## Results
+![alt text](./img/reconstructed_image.png "Reconstructed by ART")
--- demo_ART.m
+++ demo_ART.m
@@ -2,23 +2,18 @@
 % https://en.wikipedia.org/wiki/Algebraic_reconstruction_technique
 
 %% ART Equation
-% x^(k+1) = x^k + lambda * AT(b - A(x))/ATA 
+% x^(k+1) = x^k + lambda * AT(b - A(x))/ATA
 
 %%
 clear ;
-close all ;
-home ;
 
-%% GPU Processing
-% If there is GPU device on your machine, 
-% then isgpu is true. Otherwise, it is false.
-bgpu    = true;
-bfig    = true;
+isgpu   = true;
 
-%% SYSTEM SETTING
+%%  SYSTEM SETTING
 N       = 512;
+ANG     = 180;
 VIEW    = 360;
-THETA   = linspace(0, 180, VIEW + 1);   THETA(end) = [];
+THETA   = linspace(0, ANG, VIEW + 1);   THETA(end) = [];
 
 A       = @(x) radon(x, THETA);
 AT      = @(y) iradon(y, THETA, 'none', N)/(pi/(2*length(THETA)));
@@ -31,16 +26,13 @@ p       = A(x);
 x_full  = AINV(p);
 
 %% LOW-DOSE SINOGRAM GENERATION
-i0              = 1e4;
-pn              = exp(-p);
-pn              = i0.*pn;
-pn              = poissrnd(pn);
-pn              = max(-log(max(pn,1)./i0),0);
-pn(isnan(pn))   = 0;
-pn(isinf(pn))   = 0;
+i0      = 5e4;
+pn      = exp(-p);
+pn      = i0.*pn;
+pn      = poissrnd(pn);
+pn      = max(-log(max(pn,1)./i0),0);
 
-y               = pn;
-% y       = p;
+y       = pn;
 
 %% Algebraic Reconstruction Technique (ART) INITIALIZATION
 x_low   = AINV(y);
@@ -49,15 +41,15 @@ lambda  = 1e0;
 niter   = 2e2;
 bpos    = true;
 
-%% Run Algebraic Reconstruction Technique (ART)
-if bgpu
-    y   = gpuArray(y);
-    x0  = gpuArray(x0);
+%% RUN Algebraic Reconstruction Technique (ART)
+if isgpu
+    y = gpuArray(y);
+    x0 = gpuArray(x0);
 end
 
-x_art   = ART(A, AT, y, x0, lambda, niter, bpos, bfig);
+x_art   = ART(A, AT, y, x0, lambda, niter, bpos);
 
-%% CALCUATE QUANTIFICATION FACTOR 
+%% CALCUATE QUANTIFICATION FACTOR
 x_low       = max(x_low, 0);
 x_art       = max(x_art, 0);
 nor         = max(x(:));
@@ -65,8 +57,8 @@ nor         = max(x(:));
 mse_x_low   = immse(x_low./nor, x./nor);
 mse_x_art   = immse(x_art./nor, x./nor);
 
-psnr_x_low 	= psnr(x_low./nor, x./nor);
-psnr_x_art 	= psnr(x_art./nor, x./nor);
+psnr_x_low  = psnr(x_low./nor, x./nor);
+psnr_x_art  = psnr(x_art./nor, x./nor);
 
 ssim_x_low  = ssim(x_low./nor, x./nor);
 ssim_x_art  = ssim(x_art./nor, x./nor);
@@ -74,13 +66,21 @@ ssim_x_art  = ssim(x_art./nor, x./nor);
 
 %% DISPLAY
 wndImg  = [0, 0.03];
+wndPrj  = [0, 6];
 
-figure('name', 'Algebraic Reconstruction Technique (ART)');
+figure(1);
 colormap(gray(256));
 
-suptitle('Algebraic Reconstruction Technique (ART)');
-subplot(221);   imagesc(x,     	wndImg); 	axis image off;     title('ground truth');
-subplot(222);   imagesc(x_full, wndImg);   	axis image off;     title(['full-dose_{FBP, view : ', num2str(VIEW) '}']);
-subplot(223);   imagesc(x_low,  wndImg);   	axis image off;     title({['low-dose_{FBP, view : ', num2str(VIEW) '}'], ['MSE : ' num2str(mse_x_low, '%.4e')], ['PSNR : ' num2str(psnr_x_low, '%.4f')], ['SSIM : ' num2str(ssim_x_low, '%.4f')]});
-subplot(224);   imagesc(x_art,  wndImg);  	axis image off;     title({['recon_{ART, view : ', num2str(VIEW) '}'], ['MSE : ' num2str(mse_x_art, '%.4e')], ['PSNR : ' num2str(psnr_x_art, '%.4f')], ['SSIM : ' num2str(ssim_x_art, '%.4f')]});
+% suptitle('Algebraic Reconstruction Technique');
+subplot(241);   imagesc(x,      wndImg);    axis image off;     title(['ground truth']);
+
+subplot(242);   imagesc(x_full, wndImg);    axis image off;     title(['full-dose_{view : ', num2str(VIEW) '}']);
+subplot(246);   imagesc(p, wndPrj);         xlabel(['Angle ( \Delta\theta : ' num2str(ANG/VIEW) ' \circ )']);   ylabel('Detector'); title(['full-dose_{view : ', num2str(VIEW) '}']);
+
+subplot(243);   imagesc(x_low,  wndImg);    axis image off;     title({['low-dose_{view : ', num2str(VIEW) '}' ' using I0 = ' num2str(i0, '%.2e') ], ['MSE : ' num2str(mse_x_low, '%.4e')], ['PSNR : ' num2str(psnr_x_low, '%.4f')], ['SSIM : ' num2str(ssim_x_low, '%.4f')]});
+subplot(247);   imagesc(y,  wndPrj);        xlabel(['Angle ( \Delta\theta : ' num2str(ANG/VIEW) ' \circ )']);   ylabel('Detector'); title(['low-dose_{view : ', num2str(VIEW) '} using I0 = ' num2str(i0, '%.2e')]);
+
+subplot(244);   imagesc(x_art,  wndImg);    axis image off;     title({['recon_{art}'], ['MSE : ' num2str(mse_x_art, '%.4e')], ['PSNR : ' num2str(psnr_x_art, '%.4f')], ['SSIM : ' num2str(ssim_x_art, '%.4f')]});
+subplot(248);   imagesc(y - p);             xlabel(['Angle ( \Delta\theta : ' num2str(ANG/VIEW) ' \circ )']);   ylabel('Detector'); title('full-dose - low-dose');
+
 
